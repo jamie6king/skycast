0a6d1a78e9591c1bfbb43f41fc7d70bed84c7e5b
Jamie King <jamiedanielking@gmail.com>
2024-11-19 14:35:26 +0000

    Fixed React Router warnings

--- client.jsx
+++ client.jsx
@@ -21,7 +21,16 @@ const router = createBrowserRouter([
         path: "/location",
         element: <Weather />,
     },
-]);
+], {
+    future: {
+        v7_startTransition: true,
+        v7_fetcherPersist: true,
+        v7_normalizeFormMethod: true,
+        v7_partialHydration: true,
+        v7_skipActionErrorRevalidation: true,
+        v7_relativeSplatPath: true
+    }
+});
 
 // render react
 root.render(
